46c6d4b3869ec0b790cd716553b97fec1ea6278a
Shivam Sharma <uic.17mca8010@gmail.com>
2021-11-03 17:25:29 +0530

    test

--- src/test/java/com/poc/FilterTest.java
+++ src/test/java/com/poc/FilterTest.java
@@ -9,6 +9,7 @@ import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,11 +28,11 @@ public class FilterTest {
 
 	@Autowired
 	private FilterService filterService;
-
 	@Test
 	void searchFilter() {
-		when(filterRepo.filterData("shivam")).thenReturn(Stream
-				.of(new User(1, "shivam", "s", "Sharma", "Shivam@gmail.com", "male", new Date(1996, 8, 20),
+		String input="shivam";
+		when(filterRepo.filterData(input)).thenReturn(Stream
+				.of(new User(1, "Shivam", "s", "Sharma", "Shivam@gmail.com", "male", new Date(1996, 8, 20),
 						new Date(2021, 8, 20), "9876543212", "India", "Jharkhand", "Ranchi", "834001", false))
 				.collect(Collectors.toList()));
 		assertEquals(1, filterService.searchFilter("shivam").size());
